ae244cabe649db3a352c6e0717853cec040b89ef
juanm <juanmarcos2204@gmail.com>
2026-09-25 11:37:49 -0300

    Cambios en los repos y agregué capa de servicio, falta completar el servicio, y agregar una nueva interfaz

---
+++ JVMGYM.Servicio/ClientesServicio.cs
@@ -0,0 +1,34 @@
+﻿using JVMGYM.Datos.Interfaces;
+using JVMGYM.Entidades;
+namespace JVMGYM.Servicio
+{
+    public class ClientesServicio : IClientesRepositorio
+    {
+        public void Agregar(Entidades.Clientes clientes)
+        {
+            
+        }
+
+        public void Editar(Entidades.Clientes clientes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar(Entidades.Clientes clientes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entidades.Clientes ObtenerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entidades.Clientes> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
